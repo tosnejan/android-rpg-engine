22f5dc3779d06d620f8bca2cd1d6fc586c7facd0
Tošner, Jan <tosnejan@fel.cvut.cz>
2019-05-18 18:35:29 +0200

    dialog update

--- android/app/src/main/java/meletos/rpg_game/dialog/DialogSwitcher.java
+++ android/app/src/main/java/meletos/rpg_game/dialog/DialogSwitcher.java
@@ -4,11 +4,26 @@ import meletos.rpg_game.GameHandler;
 import meletos.rpg_game.characters.FatherCharacter;
 
 public class DialogSwitcher {
-    public boolean played;
+    private boolean played;
+    private int[] itemsToCheck;
+    private int[] itemsToDelete;
+    private int[] itemsToGive;
 
     public boolean check(GameHandler gameHandler, FatherCharacter character){
         if (played){
-            return character.isPlayed();
+            if (!character.isPlayed()) return false;
+        }
+
+        for (int i = 0; i < itemsToCheck.length; i++) {
+            if(!gameHandler.getInventory().hasItem(itemsToCheck[i])) return false;
+        }
+
+        for (int i = 0; i < itemsToDelete.length; i++) {
+            gameHandler.getInventory().deleteItem(itemsToDelete[i]);
+        }
+
+        for (int i = 0; i < itemsToGive.length; i++) {
+            gameHandler.getInventory().putItem(itemsToGive[i]);
         }
         return true;
     }
